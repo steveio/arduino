4a19ba7686fbea10ee6bf4e69df8cd9a36f7d697
steveio.com <info@steveio.com>
2020-04-25 21:03:05 +0100

    Websocket Server / Web Client

--- python/wsESP8266RotaryEncoderServo.py
+++ python/wsESP8266RotaryEncoderServo.py
@@ -7,11 +7,12 @@ import json
 import logging
 import websockets
 import struct
+from pprint import pprint
 
 
 logging.basicConfig()
 
-STATE = {"angle": 0}
+STATE = {"value": 0}
 
 USERS = set()
 
@@ -26,11 +27,10 @@ def users_event():
 
 async def notify_state():
     if USERS:  # asyncio.wait doesn't accept an empty list
-        message = state_event()
-        my_bytes = bytearray()
-        my_bytes.append(STATE['value'],"utf-8")
-        await asyncio.wait([user.send(message) for user in USERS])
-        await asyncio.wait([user.send(my_bytes) for user in USERS]) // send_binary
+        binary_data = struct.pack("Ii", 12, STATE['value'])
+        await asyncio.wait([user.send(binary_data) for user in USERS])
+        ###message = state_event()
+        ###await asyncio.wait([user.send(message) for user in USERS])
 
 
 async def notify_users():
@@ -39,6 +39,19 @@ async def notify_users():
         await asyncio.wait([user.send(message) for user in USERS])
 
 
+"""
+How to Client ID from 'request_headers':
+-- browser
+('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
+('Origin', 'http://127.0.0.1:3001')
+('Sec-WebSocket-Key', 'd+NSm4Dl+wLV5Q+naRlPMA==')
+
+-- arduino
+('User-Agent', 'arduino-WebSocket-Client')
+('Origin', 'file://')
+('Sec-WebSocket-Key', 'b9xcFehjFpQyrcbyWsGUQA==')
+('Sec-WebSocket-Protocol', 'arduino')
+"""
 async def register(websocket):
     USERS.add(websocket)
     await notify_users()
@@ -55,22 +68,31 @@ async def wsApi(websocket, path):
     try:
         await websocket.send(state_event())
         async for message in websocket:
-            print(type(message))
+            print('Sec-WebSocket-Key: '+websocket.request_headers['Sec-WebSocket-Key'])
+            print('MessageType: '+str(type(message)))
+            print(message);
             if isinstance(message, (bytes, bytearray)):
                 ### Binary: byte array
                 ### https://docs.python.org/3.5/library/struct.html
                 ### <class 'bytes'>
                 ### b'\x0c\x00\x00\x00@\x00\x00\x00'
                 ### (12, 64)
-                print(message);
                 tuple_of_data = struct.unpack("Ii", message)
                 print(tuple_of_data)
                 cmd = tuple_of_data[0]
                 value = tuple_of_data[1]
-            elif isinstance(message, (string)):
+                STATE["value"] = value
+                await notify_state()
+            elif isinstance(message, (str)):
                 try:
                     data = json.loads(message)
-                    if data["cmd"] == "set":
+                    if data["action"] == "minus":
+                        STATE["value"] -= 1
+                        await notify_state()
+                    elif data["action"] == "plus":
+                        STATE["value"] += 1
+                        await notify_state()
+                    elif data["action"] == "set":
                         STATE["value"] = int(data["value"])
                         await notify_state()
                     else:
