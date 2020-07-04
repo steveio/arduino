8845c4edd6f792874b6fb2bb754313ebd93a145e
steveio.com <info@steveio.com>
2020-07-04 22:18:26 +0100

    ESP32 I2C Peripheral Scanner

---
+++ ESP32I2CScanner/ESP32I2CScanner.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+
+/**
+
+Scanning...
+I2C device found at address 0x3C
+I2C device found at address 0x76
+done
+
+ */
+
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+  delay(5000);          
+}
