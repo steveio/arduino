e84ad9d55a15ce1e7e711c9ee27509a73b1cbc64
steveio.com <info@steveio.com>
2020-09-21 17:14:45 +0100

    4 button control UI

--- PlantWaterSoilSensorPump/PlantWaterSoilSensorPump.ino
+++ PlantWaterSoilSensorPump/PlantWaterSoilSensorPump.ino
@@ -2,9 +2,10 @@
  * Automated Plant Watering
  * 
  * Arduino Pro Mini 3.3v 
- * Capacitive soil moisture sensor v1.2
- * 5v water pump attached via a relay (wired normally closed)
+ * Capacitive soil moisture sensor v1.2 (pin A0)
+ * 5v water pump attached via a relay (pin d7 wired normally closed)
  * OLED SSD1306 124*64 display
+ * 4 push button control panel (wired pin d2-d6)
  * 5v power (from solar / 12v adapter) via L7805 regulator
  * 
  * When soil moisture level is found high (dry) pump is activated for n secs
@@ -24,7 +25,8 @@ const int WaterValue = 570;
 const int soilSensorPin = A0;
 unsigned long startTime = 0;
 const unsigned long calibrationDelay = 30000; // time for sensor to calibrate (level)
-int intervals = (AirValue - WaterValue)/3;
+int intervalDivider = 2; // 1-5   
+int intervals = (AirValue - WaterValue)/intervalDivider;
 int soilMoistureValue = 0;
 
 int soilMoistureStatusId;
@@ -56,33 +58,122 @@ unsigned long pumpActivations = 0;
 
 SSD1306AsciiWire oled;
 
+bool displayActive = 0; // display active true/false
+unsigned long displayTimeout = 300000; // delay to turn display off
+unsigned long displayLastActivation = 0; // ts of last display activation
+
+#define DISPLAY_STATUS 0x01
+
+
+volatile bool irqStatus = 0;
+volatile int activeButton = 0;
+
+// buttons correspond to digital pin set bit in PORTD D3-D6
+// PIND 0 (01), 1 (02), 2 (04), 3 b1 D3 (0x08), 4 b2 D4 (0x10), 5 b3 D5 (0x20), 6 b4 D6 (0x40)
+
+#define BUTTON_01 0x08
+#define BUTTON_02 0x10
+#define BUTTON_03 0x20
+#define BUTTON_04 0x40
+
+unsigned long buttonDebounce = 150; // msecs
+unsigned long buttonLastActive = 0;
+
+// push button IRQ
+void pin2IRQ()
+{
+  int x;
+  x = PIND; // digital pins 0-7
+
+  if ((x & BUTTON_01) == 0) {
+    activeButton = BUTTON_01;
+  }
+
+  if ((x & BUTTON_02) == 0) {
+    activeButton = BUTTON_02;
+  }
+
+  if ((x & BUTTON_03) == 0) {
+    activeButton = BUTTON_03;
+  }
+
+  if ((x & BUTTON_04) == 0) {
+    activeButton = BUTTON_04;
+  }
+
+  if (activeButton > 0)
+  {
+    irqStatus = 1;
+  }
+}
+
+// button event handler
+void handleButtonEvent()
+{
+  if (millis() > buttonLastActive + buttonDebounce)
+  {
+    switch (activeButton)
+    {
+      case BUTTON_01:
+        Serial.println("Button 1: ");
+        break; 
+      case BUTTON_02:
+        Serial.println("Button 2: Pump on/off");
+        (pumpStatus == 0) ? pumpOn() : pumpOff();
+        break;
+      case BUTTON_03:
+        Serial.println("Button 3:");
+        break;
+      case BUTTON_04:
+        Serial.println("Button 4:");
+        break;
+      default:
+        break;
+    }
+  }
+
+  activeButton = 0;
+  buttonLastActive = millis();
+  irqStatus = 0;
+}
 
 bool isCalibrating()
 {
   return (millis() > startTime + calibrationDelay) ? false : true;
 }
 
-void activatePump()
+void pumpOn()
 {
-  if(!isCalibrating() && (pumpLastActivation == 0 || millis() > pumpLastActivation + pumpDelay))
-  {
+    pumpStatus = 1;
+    digitalWrite(relayPin, LOW);  
+
     Serial.print(millis());
     Serial.print("\t");
     Serial.println("Pump On:");
+}
 
-    pumpStatus = 1;
-    displayStatus();
-
-    digitalWrite(relayPin, LOW);
-    delay(pumpActiveTime);
+void pumpOff()
+{
+    pumpStatus = 0;
     digitalWrite(relayPin, HIGH);
 
     Serial.print(millis());
     Serial.print("\t");
     Serial.println("Pump Off:");
+}
 
-    pumpStatus = 0;
-    displayStatus();
+// scheduled pump activation for duration pumpActiveTime
+void activatePump()
+{
+  if(!isCalibrating() && (pumpLastActivation == 0 || millis() > pumpLastActivation + pumpDelay))
+  {
+    pumpOn();
+    display(DISPLAY_STATUS);
+
+    delay(pumpActiveTime);
+
+    pumpOff();        
+    display(DISPLAY_STATUS);
 
     pumpActivations++;
     pumpLastActivation = millis();
@@ -122,44 +213,46 @@ void readSoilMoisture()
     activatePump();
   }
 
-  displayStatus();
+  display(DISPLAY_STATUS);
 }
 
-void displayStatus()
+void display(int opt)
 {
 
   oled.setFont(Adafruit5x7);
-
   oled.clear();
-  oled.println("Soil Moisture: ");
-  oled.set2X();
-  if (isCalibrating())
-  {
-    oled.println("Calibrating..");
-  } else {
-    oled.print(soilMoistureValue);
-    oled.print(" ");
-    oled.println(label[soilMoistureStatusId]);
-  }
-  oled.set1X();
-  oled.println();
-  oled.print("Pump:");
-  oled.print(pumpStatus);
-  oled.print(" / ");
-  oled.println(pumpActivations);
 
-  oled.print("Pump Last:");
-  if (pumpLastActivation > 0)
+  if (opt == DISPLAY_STATUS)
   {
-    oled.println((millis() - pumpLastActivation) / 1000);
-  } else {
-    oled.println(0);
+    oled.println("Soil Moisture: ");
+    oled.set2X();
+    if (isCalibrating())
+    {
+      oled.println("Calibrating..");
+    } else {
+      oled.print(soilMoistureValue);
+      oled.print(" ");
+      oled.println(label[soilMoistureStatusId]);
+    }
+    oled.set1X();
+    oled.println();
+    oled.print("Pump:");
+    oled.print(pumpStatus);
+    oled.print(" / ");
+    oled.println(pumpActivations);
+  
+    oled.print("Pump Last:");
+    if (pumpLastActivation > 0)
+    {
+      oled.println((millis() - pumpLastActivation) / 1000);
+    } else {
+      oled.println(0);
+    }
+    oled.print("Dur/Delay:");
+    oled.print(pumpActiveTime / 1000);
+    oled.print(" / ");
+    oled.println(pumpDelay / 1000);
   }
-  oled.print("Dur/Delay:");
-  oled.print(pumpActiveTime / 1000);
-  oled.print(" / ");
-  oled.println(pumpDelay / 1000);
-
 }
 
 void setup() {
@@ -168,6 +261,7 @@ void setup() {
 
   delay(2000);
 
+  // Init..
   startTime = millis();
   Serial.print("Start ts: ");
   Serial.println(startTime);
@@ -181,11 +275,12 @@ void setup() {
   Serial.print("Pump Delay: ");
   Serial.println(pumpDelay);
 
+  // Sensor / Pump (relay) Pins
   pinMode(soilSensorPin, INPUT);
-
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, HIGH); // switch pump off
 
+  // OLED LCD Display
   Wire.begin();
   Wire.setClock(400000L);
 
@@ -195,9 +290,22 @@ void setup() {
     oled.begin(&Adafruit128x64, I2C_ADDRESS);
   #endif // RST_PIN >= 0
 
+  // Buttons & IRQ
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+
+  attachInterrupt(0, pin2IRQ, FALLING);
+
 }
 
 void loop() {
+  if (irqStatus == 1)
+  {
+    handleButtonEvent();
+  }
   readSoilMoisture();
-  delay(100);
+  delay(200);
 }
